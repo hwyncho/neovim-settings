e9d02281acce569baf79feb9881444a287e624bd
Hwiyeon Cho <hwyn.cho@gmail.com>
2026-02-03 12:29:14 +0900

    [fix] Update treesitter config for new API
    - nvim-treesitter 새 API에 맞게 설정 변경
    - nvim-treesitter.configs 모듈이 제거되어 nvim-treesitter 모듈 사용
    - textobjects 플러그인을 별도 스펙으로 분리
    - incremental selection 키맵 수동 설정으로 변경

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,9 +3,8 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-        dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
         config = function()
-            require("nvim-treesitter.configs").setup {
+            require("nvim-treesitter").setup {
                 ensure_installed = {
                     "lua",
                     "vim",
@@ -27,57 +26,91 @@ return {
                     "dockerfile",
                 },
                 auto_install = true,
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-                indent = {
-                    enable = true,
-                },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "<C-space>",
-                        node_incremental = "<C-space>",
-                        scope_incremental = false,
-                        node_decremental = "<bs>",
+            }
+
+            -- Treesitter-based folding (optional)
+            vim.opt.foldmethod = "expr"
+            vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+            vim.opt.foldenable = false
+
+            -- Incremental selection keymaps
+            local map = vim.keymap.set
+            map("n", "<C-space>", function()
+                require("nvim-treesitter.incremental_selection").init_selection()
+            end, { desc = "Init treesitter selection" })
+            map("x", "<C-space>", function()
+                require("nvim-treesitter.incremental_selection").node_incremental()
+            end, { desc = "Increment treesitter selection" })
+            map("x", "<bs>", function()
+                require("nvim-treesitter.incremental_selection").node_decremental()
+            end, { desc = "Decrement treesitter selection" })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        event = { "BufReadPost", "BufNewFile" },
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            require("nvim-treesitter-textobjects").setup {
+                select = {
+                    lookahead = true,
+                    selection_modes = {
+                        ["@function.outer"] = "V",
+                        ["@class.outer"] = "V",
                     },
                 },
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["ac"] = "@class.outer",
-                            ["ic"] = "@class.inner",
-                            ["aa"] = "@parameter.outer",
-                            ["ia"] = "@parameter.inner",
-                        },
-                    },
-                    move = {
-                        enable = true,
-                        set_jumps = true,
-                        goto_next_start = {
-                            ["]m"] = "@function.outer",
-                            ["]]"] = "@class.outer",
-                        },
-                        goto_next_end = {
-                            ["]M"] = "@function.outer",
-                            ["]["] = "@class.outer",
-                        },
-                        goto_previous_start = {
-                            ["[m"] = "@function.outer",
-                            ["[["] = "@class.outer",
-                        },
-                        goto_previous_end = {
-                            ["[M"] = "@function.outer",
-                            ["[]"] = "@class.outer",
-                        },
-                    },
+                move = {
+                    set_jumps = true,
                 },
             }
+
+            -- Textobject keymaps
+            local map = vim.keymap.set
+            map({ "x", "o" }, "af", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+            end, { desc = "Select outer function" })
+            map({ "x", "o" }, "if", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+            end, { desc = "Select inner function" })
+            map({ "x", "o" }, "ac", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+            end, { desc = "Select outer class" })
+            map({ "x", "o" }, "ic", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
+            end, { desc = "Select inner class" })
+            map({ "x", "o" }, "aa", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@parameter.outer", "textobjects")
+            end, { desc = "Select outer parameter" })
+            map({ "x", "o" }, "ia", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@parameter.inner", "textobjects")
+            end, { desc = "Select inner parameter" })
+
+            -- Move keymaps
+            local move = require "nvim-treesitter-textobjects.move"
+            map({ "n", "x", "o" }, "]m", function()
+                move.goto_next_start("@function.outer", "textobjects")
+            end, { desc = "Next function start" })
+            map({ "n", "x", "o" }, "]M", function()
+                move.goto_next_end("@function.outer", "textobjects")
+            end, { desc = "Next function end" })
+            map({ "n", "x", "o" }, "]]", function()
+                move.goto_next_start("@class.outer", "textobjects")
+            end, { desc = "Next class start" })
+            map({ "n", "x", "o" }, "][", function()
+                move.goto_next_end("@class.outer", "textobjects")
+            end, { desc = "Next class end" })
+            map({ "n", "x", "o" }, "[m", function()
+                move.goto_previous_start("@function.outer", "textobjects")
+            end, { desc = "Previous function start" })
+            map({ "n", "x", "o" }, "[M", function()
+                move.goto_previous_end("@function.outer", "textobjects")
+            end, { desc = "Previous function end" })
+            map({ "n", "x", "o" }, "[[", function()
+                move.goto_previous_start("@class.outer", "textobjects")
+            end, { desc = "Previous class start" })
+            map({ "n", "x", "o" }, "[]", function()
+                move.goto_previous_end("@class.outer", "textobjects")
+            end, { desc = "Previous class end" })
         end,
     },
 }
