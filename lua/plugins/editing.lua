a96428381647e2b2e6a1ae13bd4cb01359f9c30c
Hwiyeon Cho <hwyn.cho@gmail.com>
2026-04-05 09:57:04 +0900

    [fix] Disable ts_context_commentstring autocmd
    - YAML 등 파일에서 CursorHold 시 language_tree nil 에러 수정
    - Comment.nvim pre_hook 통합 시 권장 설정인 enable_autocmd = false 적용
    - Neovim 및 VSCode Neovim 양쪽 editing.lua에 동일 적용

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -2,7 +2,12 @@ return {
     {
         "numToStr/Comment.nvim",
         event = { "BufReadPre", "BufNewFile" },
-        dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+        dependencies = {
+            {
+                "JoosepAlviste/nvim-ts-context-commentstring",
+                opts = { enable_autocmd = false },
+            },
+        },
         config = function()
             require("Comment").setup {
                 pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
